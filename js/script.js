7c8744a8835d454346698a0aa48a140cb556a4ce
Anna Perkowska <perkowska.a@gmail.com>
2018-01-26 10:00:10 +0100

    Change script.js & index.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var a = 5,
 	h = 4,
-	triangleArea;
+	triangleArea,
+	a1 = prompt('Podaj wartość a1'),
+	h1 = prompt('Podaj wartość h1'),
+	triangleArea1;
 triangleArea = a*h/2;
- console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
- prompt('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+triangleArea1 = a1*h1/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Pole trójkąta to:' + triangleArea1);
